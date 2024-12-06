d8adde6fb0db0cdb3535bed710796b763fe7a4ce
Matthew Portman <29168957+MatthewPortman@users.noreply.github.com>
2024-12-05 23:43:09 -0600

    Added animation (kind of)
    Currently working on a means to animate the output but I'm having trouble... what I am committing is a very inefficient prototype given issues I had with other (non-committed) attempts.

--- visualization/generate_mosaic.py
+++ visualization/generate_mosaic.py
@@ -1,33 +1,122 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-def generate_mosaic(image_array, fig = None, ax = None, filename = "mosaic.png"):
-    if fig is None or ax is None:
-        # Clear any existing plots
-        plt.clf()
+def init():
+    plot_image.set_data(np.zeros((height, width)))
+    plot_image.set_alpha(0)
+    return plot_image,
 
-        # Create a figure and subplots
-        fig, ax = plt.subplots(1, 1, figsize = (10, 10))
+def add_galaxy_to_mosaic(frame, all_images):
+
+    image_index = frame // 3
+    image_array = all_images[image_index]
 
     # Performing a logarithmic transformation of the image data
-    scaling_constant = 255/np.log(1 + np.max(image_array))
+    #scaling_constant = 255/np.log(1 + np.max(image_array))
+
+    # For fading in
+    # Gradually increase alpha from 0 to 1 after initial frame
+    if frame == 0:
+        alpha = 1
+
+    elif frame % 3 == 0:
+        alpha = 0
+
+    #elif frame % 3 == 1:
+    #    alpha = 0.5
+
+    elif (frame % 3) != 0:
+        alpha = min((frame / 3), 1)
+
+    else:
+        alpha = 1
+
+    #plot_image.set_alpha(alpha)
 
     # Display the FITS image after scaling
-    # image_fits = ax.imshow(scaling_constant * np.log(1 + image_data_fits), cmap='gray')
-    _ = ax.imshow(scaling_constant * np.log(1 + image_array), cmap='gray')
-
-    ax.set_title('Galaxy Mosaic')
-    # ax.set_xlabel('X-axis')
-    # ax.set_ylabel('Y-axis')
-    ax.invert_yaxis()
+    plot_image = ax.imshow(image_array, cmap = 'gray', alpha = alpha)
+    # _ = ax.imshow(scaling_constant * np.log(1 + image_array), cmap='gray')
 
     # Show the plot
     # plt.show()
 
     # Save the plot
-    _ = fig.savefig(filename, dpi = fig.dpi, bbox_inches = 'tight')
+    #_ = fig.savefig(filename, dpi = fig.dpi, bbox_inches = 'tight')
 
-    return fig, ax
+    return plot_image,
+
+def generate_animation(
+        all_images,
+        time_step,
+        filename = "animation.gif"
+):
+    # Clear any existing plots
+    plt.clf()
+    # Create a figure
+    #global ax, plot_image, height, width
+    fig, ax = plt.subplots()
+
+    ax.set_title('Galaxy Mosaic')
+    # ax.set_xlabel('X-axis')
+    # ax.set_ylabel('Y-axis')
+    ax.invert_yaxis()
+    # Create an animation
+
+    #height, width = np.shape(all_images[0])
+    #plot_image = ax.imshow(np.zeros((height, width)), cmap = "gray", alpha = 1)
+
+    # ims is a list of lists, each row is a list of artists to draw in the
+    # current frame; here we are just animating one artist, the image, in
+    # each frame
+    ims = []
+    previous_images = []
+
+    # show an initial image first
+    ax.imshow(all_images[0], alpha = 1, cmap = "gray")
+
+    num_images = len(all_images)
+    dim = num_images * 2
+    initial_alphas = np.zeros((dim, dim))
+    lower_triangular = np.tril_indices(dim)
+    # lower_triangular_add_on = np.tril_indices(cols - 1)
+    lowest_row = [1 - (i / dim) for i in range(dim)]
+    add_on = np.tile(lowest_row, (dim, 1))
+    for pos in zip(*lower_triangular):
+        x = pos[0]
+        y = pos[1]
+        alpha = (x - y) / (dim - 1)
+        initial_alphas[pos] = min(alpha, 1)
+
+        add_on[x, :] += 1 / (dim - 1)
+
+    #last_add_on = np.ones(((dim - 1), dim))
+    initial_alphas = np.vstack((initial_alphas, add_on)) #, last_add_on))
+    initial_alphas[initial_alphas > 1] = 1
+
+    all_images_for_animation = np.array([all_images] * len(all_images))
+    ims = [
+        [
+            ax.imshow(
+                    all_images_for_animation[r, c],
+                    animated = True,
+                    alpha = alpha,
+                    cmap = "gray"
+            )
+        ]
+        for r, row in enumerate(initial_alphas)
+        for c, alpha in enumerate(row)
+    ]
+
+    ani = animation.ArtistAnimation(
+            fig,
+            ims,
+            #add_galaxy_to_mosaic,
+            interval = time_step * 100, # milliseconds
+            blit = True
+    )
+
+    ani.save(filename, writer = 'imagemagick')
 
 # from PIL import Image
 #
